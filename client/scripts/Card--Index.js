1478a573d01f7c271a593f5ab025639898d636ae
TkDevk <duvanuriel@hotmail.com>
2023-10-10 01:30:16 -0500

    All sheets linked, Missing Js button

--- client/scripts/Card--Index.js
+++ client/scripts/Card--Index.js
@@ -1,4 +1,6 @@
-const names = [];
+/*
+
+cnst names = [];
 const prices = [];
 const occupations = [];
 const rates = [];
@@ -29,7 +31,7 @@ class Persona{  //Para meterle parametros a una clase uso el consutrctor, toda c
             //console.log("Hola, soy " + this.nombre + " y tengo " + this.edad + " años")
     }
    /*saludar(){
-    console.log("Hola, soy " + this.nombre + " y tengo " + this.edad + " años") */
+    console.log("Hola, soy " + this.nombre + " y tengo " + this.edad + " años") 
    }
   
 } 
@@ -43,3 +45,66 @@ let pepo = new Persona("Pepo", 26);
 let pepu = new Persona("Pepu", 25);
 
 pepito.saludar();
+*/
+
+/*Random information because we don't users yet xd*/
+
+/***
+ * Api randomUser
+ * https://randomuser.me. */
+
+
+
+
+ 
+const ProfileCard = (() => {
+  const profile = document.querySelector('.card-profile-picture');
+  const name = document.querySelector('.card--user-name');
+  
+
+
+  return {
+      setData: (data) => {
+          let fullname = `${data.name.first} ${data.name.last}`;
+          profile.src = data.picture.large;
+          name.textContent = fullname;
+          /*
+          description.textContent = `Hello, I am ${fullname} from ${origin}. To learn more about me, my username is` +
+          `${data.login.username}. Feel free to email me at ${data.email} !`;
+*/
+      }
+  }
+})();
+
+fetch('https://randomuser.me/api/?results=1'); //Fecth al website
+then((response) => resp.json()) // Entonces, mi parametro va a ser response, que va a ser un objeto json, espero respuesta then
+then((data)=>{
+ ProfileCard.setData(data.results[0]);//Function with queryselectors would be Profile card
+});
+
+
+
+function showIcon() {
+  let buttonBurger = document.getElementById("bx-menu");
+  formulario.style.display = "block";
+}
+
+function hideIcon() {
+  let buttonBurger = document.getElementById("bx-menu");
+
+  formulario.style.display = "none";
+}
+
+// Función para mostrar y ocultar los formularios del perfil
+function showIcon(indice) {
+  const formularios = document.querySelectorAll('.header .header__nav');
+  formularios.forEach((formulario) => {
+      formulario.style.display = 'none';
+  });
+  const formularioAMostrar = document.getElementById(`hiddenForm${indice}`);
+  formularioAMostrar.style.display = 'block';
+}
+
+document.addEventListener('DOMContentLoaded', function () {
+  mostrarFormulario(1);
+});
