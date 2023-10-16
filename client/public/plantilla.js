01ab56d4fcaae5af9ccf64dfa082bf4773d6f38c
TkDevk <duvanuriel@hotmail.com>
2023-10-16 17:33:51 -0500

    theme night

--- client/public/plantilla.js
+++ client/public/plantilla.js
@@ -80,7 +80,28 @@ window.addEventListener('resize', () => {
 });
 
 const toggler = document.getElementById('theme-toggle');
+const currentTheme = localStorage.getItem("theme"); //Theme is my key name, can be any value, is not the same as below because in this one i'm not saying the value that i want save i'm asumming the value with a conditional to verify if the state is active or not, getItem is a localStorage method
+if (currentTheme) {    
+    if (currentTheme === "dark") { // if currentThem has some value it means that the user has checked the option meaning that can be storage in the localStorage XD
+        document.body.classList.add('dark');
+        toggler.checked = true;
+    }
+}
+toggler.addEventListener('change', function () {
+    if (this.checked) {
+        document.body.classList.add('dark');
+        localStorage.setItem("theme", "dark"); // LocalStorage will find the theme Item instead of using the simple value document.body.classList.add('dark');
+    } else {
+        document.body.classList.remove('dark');
+        localStorage.setItem("theme", "light"); 
+    }
+});
 
+
+
+/*
+const toggler = document.getElementById('theme-toggle');
+localStorage.setItem("changeTheme",(toggler) ); // Save in localstorage
 toggler.addEventListener('change', function () {
     if (this.checked) {
         document.body.classList.add('dark');
@@ -94,9 +115,39 @@ function saveCard() {
     document.getElementById('btn').style.backgroundColor = "#ff9c7eca";
 }
 */
-const saveButton = document.querySelectorAll('.btn'); 
+
+/*
+
+const saveButton = document.querySelectorAll('.saved--icon');
+
+
+const saveButton = document.querySelectorAll('.saved--icon'); 
 saveButton.forEach(btn => {
    btn.addEventListener('click', function onClick(){
-    btn.style.backgroundColor = '#ff9c7eca'; 
+    const backgroundColor = btn.style.backgroundColor;
+
+    if (backgroundColor === '#ff9c7eca') {
+      btn.style.backgroundColor = '#ff5722';
+    } else {
+      btn.style.backgroundColor ='#ff9c7eca';
+    }
+    
  })
+});
+*/
+/**I can use the add and remove as the change aspect property Js before  */
+const saveButton = document.querySelectorAll('.saved--icon');
+saveButton.forEach(btn => {
+    btn.addEventListener('click', function onClick(){
+       
+        if (btn.classList.contains('saved--icon--selected')) {
+            btn.classList.remove('saved--icon--selected');
+            btn.classList.add('saved--icon');
+          } else {
+            btn.classList.remove('saved--icon');
+            btn.classList.add('saved--icon--selected');
+          }
+        })   
 })
+
+
