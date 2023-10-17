f2b346fd1f057d4d89dd48ee715f33364d777bb2
Gabriela <gabrielaaguiar@gmail.com>
2023-10-17 14:06:38 -0500

    Modificaciones del perfil y formularios.

--- client/scripts/formsProfile.js
+++ client/scripts/formsProfile.js
@@ -1,15 +1,21 @@
-function mostrarFormularioHelpCenter(formulario) {
-    // Oculta todos los formularios al principio
-    document.getElementById("formPersonalInfo").style.display = "none";
-    document.getElementById("formEditProfile").style.display = "none";
-    document.getElementById("formNotifications").style.display = "none";
+document.addEventListener("DOMContentLoaded", function () {
+  document.getElementById("formPersonalInfo").style.display = "none";
+  document.getElementById("formEditProfile").style.display = "none";
+  document.getElementById("formNotifications").style.display = "none";
 
-    // Muestra el formulario correspondiente
-    if (formulario === 1) {
-      document.getElementById("formPersonalInfo").style.display = "block";
-    } else if (formulario === 2) {
-      document.getElementById("formEditProfile").style.display = "block";
-    } else if (formulario === 3) {
-      document.getElementById("formNotifications").style.display = "block";
-    }
+  document.getElementById("formPersonalInfo").style.display = "block";
+});
+
+function mostrarFormularioHelpCenter(formulario) {
+  document.getElementById("formPersonalInfo").style.display = "none";
+  document.getElementById("formEditProfile").style.display = "none";
+  document.getElementById("formNotifications").style.display = "none";
+
+  if (formulario === 1) {
+    document.getElementById("formPersonalInfo").style.display = "block";
+  } else if (formulario === 2) {
+    document.getElementById("formEditProfile").style.display = "block";
+  } else if (formulario === 3) {
+    document.getElementById("formNotifications").style.display = "block";
   }
+}
