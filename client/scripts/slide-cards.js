801404d2cd2782be58a521d0525e54de966daf53
Gabriela <gabrielaaguiar@gmail.com>
2023-11-05 17:49:10 -0500

    Cambios y responsive de la pagina nosotros, index y planes.

--- client/scripts/slide-cards.js
+++ client/scripts/slide-cards.js
@@ -1,3 +1,4 @@
+// Activar botenes slide de la pagina inicio (index)
 function scrollCards(direction) {
     const container = document.querySelector('.contenedor__tarjeta');
     const scrollLeft = container.scrollLeft;
@@ -5,3 +6,28 @@ function scrollCards(direction) {
   
     container.scrollLeft = scrollLeft + (direction * cardWidth);
   }
+
+
+// Filtros de la pagina de inicio (index)
+const botonesRadio = document.querySelectorAll('input[name="categories"]');
+
+botonesRadio.forEach((botonRadio) => {
+  botonRadio.addEventListener('click', manejarFiltroCategoria);
+});
+
+function manejarFiltroCategoria() {
+  const categoriaSeleccionada = document.querySelector('input[name="categories"]:checked').value;
+  const contenedoresTarjeta = document.querySelectorAll('.card--container');
+
+  contenedoresTarjeta.forEach((contenedorTarjeta) => {
+    const categoria = contenedorTarjeta.getAttribute('data-category');
+
+    if (categoria === categoriaSeleccionada || categoriaSeleccionada === 'todo') {
+      contenedorTarjeta.style.display = 'block'; 
+      categoriaSeleccionada.style.background = '#343759';
+    } else {
+      contenedorTarjeta.style.display = 'none'; 
+      categoriaSeleccionada.style.background = 'initial';
+    }
+  });
+}
