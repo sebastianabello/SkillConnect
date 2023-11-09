4393e215441b245b86147e9b4e05b3e8dc022159
TkDevk <duvanuriel@hotmail.com>
2023-11-09 02:02:12 -0500

    sleepy

--- client/scripts/save.js
+++ client/scripts/save.js
@@ -4,44 +4,39 @@
 	
 			// Función para mostrar las tarjetas guardadas
 			function renderSavedCards() {
-				savedCardsContainer.innerHTML = ""; // Borra el contenido actual
-	
-				savedCards.forEach(function (user, index) {
-					const savedCardElement = document.createElement("article");
-					savedCardElement.className = "card--container";
-					savedCardElement.innerHTML = `
-                    <header class="card-figures">
-                    <img class="card-profile-picture" src="${user.img}">
-                    <button class="saved--icon" data-index="${index}">
-                      <i class="fa-solid fa-bookmark aside-icon"></i>
-                    </button>
-                  </header>
-                  <footer class="card--description">
-                    <h3 class="card--user-name">${user.serviceName}</h3>
-                    <span class="card--skill">${user.descriptionName}</span>
-                    <p class="card--price">Precio ${user.priceInfo}</p>
-                    <picture class="card--rate--button--container">
-                      <button id="avatar-perfil--button" type="">
-                        <i class="fa-solid fa-star"></i> 5.0
-                      </button>
-                      <button class="card--rate--button">Contactar</button>
-                    </picture>
-                  </footer>
-                `;
-	
-					// Agrega un controlador de eventos para el botón de eliminación
-					const removeButton = document.createElement("button");
-					removeButton.className = "remove-button";
-					removeButton.setAttribute("data-index", index);
-					removeButton.innerHTML = `<button class="card--rate--button">X</button>`;
-					removeButton.addEventListener("click", function() {
-						removeSavedCard(index);
-					});
-					savedCardElement.querySelector(".card--description").appendChild(removeButton);
-	
-					savedCardsContainer.appendChild(savedCardElement);
-				});
-			}
+                savedCardsContainer.innerHTML = ""; // Borra el contenido actual
+            
+                savedCards.forEach(function (user, index) {
+                    const savedCardElement = document.createElement("article");
+                    savedCardElement.className = "card--container";
+                    savedCardElement.innerHTML = `
+                        <header class="card-figures">
+                            <img class="card-profile-picture" src="${user.img}">
+                            <button class="saved--icon">
+                            <i class="fa-solid fa-trash"></i>
+                            </button>
+                        </header>
+                        <footer class="card--description">
+                            <h3 class="card--user-name">${user.serviceName}</h3>
+                            <span class="card--skill">${user.descriptionName}</span>
+                            <p class="card--price">Precio ${user.priceInfo}</p>
+                            <picture class="card--rate--button--container">
+                                <button class="card--rate--button">Contactar</button>
+                            </picture>
+                        </footer>
+                    `;
+            
+                    savedCardsContainer.appendChild(savedCardElement);
+                });
+            
+                // Agrega un controlador de eventos al contenedor principal para manejar la eliminación
+                savedCardsContainer.addEventListener("click", function(event) {
+                    if (event.target.classList.contains("saved--icon")) {
+                        const cardIndexToRemove = parseInt(event.target.getAttribute("data-index"), 10);
+                        removeSavedCard(cardIndexToRemove);
+                    }
+                });
+            }
 	
 			// Agrega un controlador de eventos para el botón de eliminación
 			savedCardsContainer.addEventListener("click", function(event) {
